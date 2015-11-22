18bdd8284a9b68648ed7b43f7cf4832ec14c3886
keisuke <keisuke495500@gmail.com>
2015-11-22 10:17:36 +0900

    複数経路の作成ができた、calcRoute関数にまとめた

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SapporoOpenMap
 札幌オープンデータハッカソン用
+# 関数の使い方
+* calcRoute(start,end,waylist)
+** start...開始地点
+** end...到着地点
+** waylist...経由地点[{location:○○},{location:xx}...]のように入力する
--- 経路情報担当/main.js
+++ 経路情報担当/main.js
@@ -5,31 +5,30 @@ function initMap() {
     scrollwheel: false,
     zoom: 17
   });
-  var start = '札幌駅';
-  var end = '白石駅';
-  var waylist = [{location: '大通駅'},{location: "すすきの駅"}]
-
-  var directionsDisplay = new google.maps.DirectionsRenderer({
-    map: map
-  });
-
-  // Set destination, origin and travel mode.
-  var request = {
-    destination: start,//{lat: 43.0620341,lng: 141.3521904},
-    origin: end,//{lat: 43.0612343,lng: 142.3499813},
-    waypoints: waylist,
-    travelMode: google.maps.TravelMode.DRIVING
-  };
-
-  // Pass the directions request to the directions service.
-  var directionsService = new google.maps.DirectionsService();
-  directionsService.route(request, function(response, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      // Display the route on the map.
-      directionsDisplay.setDirections(response);
-    }
-    else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
+  calcRoute('札幌　ビッグカメラ','札幌　ヨドバシカメラ');
+  calcRoute('札幌駅','札幌　紀伊國屋書店');
+  calcRoute('大通駅','北海道庁',[{location: '白石駅'}]);
+  function calcRoute(start, end, waylist){
+    var directionsDisplay = new google.maps.DirectionsRenderer({
+      map: map
+    });
+    // Set destination, origin and travel mode.
+    var request = {
+      destination: start,//{lat: 43.0620341,lng: 141.3521904},
+      origin: end,//{lat: 43.0612343,lng: 142.3499813},
+      waypoints: waylist,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+    // Pass the directions request to the directions service.
+    var directionsService = new google.maps.DirectionsService();
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        // Display the route on the map.
+        directionsDisplay.setDirections(response);
+      }
+      else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
 }
